fe60d6fc1940f023b7b37976fabccb275b9808f3
frebern <my@email.com>
2016-06-13 17:46:33 +0900

    shuttle_current.js now up-to-date.

--- hyumini/bus/shuttle_current.js
+++ hyumini/bus/shuttle_current.js
@@ -6,10 +6,18 @@
  * shuttle.php로 ajax를 보내 현재 운행중인 제일 가까운 셔틀 시간을 가져옵니다
  * 가져온 정보는 테이블로 바꿔서 출력합니다.
  * ( <div id="shuttleNow"></div> 안에 출력합니다. )
+ * 에러 발생시, 에러 문구를 출력합니다.
  * 
  */
 
 $(document).ready(function(){
+
+		function showMessage(msg){
+			var p = $(document.createElement("p"));
+			p.attr("id", "shuttleNowMessage");
+			p.text(msg);
+			$("#shuttleNow").html(p);
+		}
 
 		function remainMinutes(time){
 			time = time.split(":");
@@ -26,10 +34,13 @@ $(document).ready(function(){
 				var courses = Object.keys(table);
 //				console.log(table);
 				var nowTable = $(document.createElement("table"));
+				var caption = $(document.createElement("caption"));
+				caption.text("버스가 오기까지...");
 				nowTable.attr("id", "shuttleNowTable");
+				nowTable.append(caption);
 				$.map(table, function(stations, course){				
 					var tr_head = $(document.createElement("tr"));
-					var td_head = $(document.createElement("td"));
+					var td_head = $(document.createElement("th"));
 
 					td_head.attr("colspan", stations.length);
 					td_head.text(course.replace("_","에서 "));
@@ -51,11 +62,13 @@ $(document).ready(function(){
 			}
 		}
 		function on400(json){
-
+			showMessage("리퀘스트 에러가 발생했습니다.");
 		}
 		function on404(json){
+			showMessage("현재 운행중인 셔틀버스가 없습니다.");
 		}
 		function on500(json){
+			showMessage("서버 에러가 발생했습니다.");
 		}
 
 		function shuttleNow(){
